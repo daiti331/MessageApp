f12457be8e91f4eaac7cb687a46a5627ec9dfaa6
daiti331 <shukatu331@gmail.com>
2025-04-03 18:51:28 +0900

    カスタム環境変数

--- MessageApp/src/main/java/com/example/messageapp/service/EmailService.java
+++ MessageApp/src/main/java/com/example/messageapp/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.example.messageapp.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,13 +11,17 @@ import lombok.RequiredArgsConstructor;
 public class EmailService {
 
     private final JavaMailSender emailSender;
+    // application.yml で定義した base-url をインジェクション
+    @Value("${custom.base-url}")
+    private String baseUrl;
 
     public void sendConfirmationEmail(String to, String token) {
         SimpleMailMessage message = new SimpleMailMessage();
+        String confirmationUrl = baseUrl + "/confirm?token=" + token; // base-urlを使ってリンク生成
         message.setTo(to);
         message.setSubject("メールアドレス確認");
         message.setText("以下のリンクをクリックしてメールアドレスを確認してください:\n"
-                + "http://localhost:8080/confirm?token=" + token);
+                + confirmationUrl);
         message.setFrom("info@earth-sys.com");
         emailSender.send(message);
     }
